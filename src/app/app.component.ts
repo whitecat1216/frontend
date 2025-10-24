8350baa2a41bbd6f22659b5619dd69fb28d71095
whitecat1216 <yuukikotani1216@gmail.com>
2025-10-24 19:12:18 +0900

    [new]angular側の環境構築

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { EmployeeService } from './services/employee.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],
+  templateUrl: './app.component.html',
+  providers: [EmployeeService]
+})
+export class AppComponent {
+  employees: any[] = [];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit() {
+    this.employeeService.getEmployees().subscribe(data => {
+      this.employees = data;
+    });
+  }
+}
---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,12 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class EmployeeService {
+  private baseUrl = "http://localhost:8080/api/employees";
+  constructor(private http: HttpClient) {}
+  getEmployees(): Observable<any[]> {
+    return this.http.get<any[]>(this.baseUrl);
+  }
+}
